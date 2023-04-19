55fa32d941cc180a5895dc357a36b33c37e4c376
unknown <2214290009@academico.iesb.br>
2023-04-18 21:31:55 -0300

    Importar o restante dos arquivos

--- construcaofrontend/src/App.jsx
+++ construcaofrontend/src/App.jsx
@@ -1,17 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Principal from "./components/Principal";
+
 export default function App() {
 
   return (
     <>
-      <header>
-        <h1>Meu Site React</h1>
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Meus Pedidos</a></li>
-            <li><a href="#">Sobre</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
+      <Principal />
+      <Footer />
     </>
   )
 }
---
+++ construcaofrontend/src/components/Principal.jsx
@@ -0,0 +1,18 @@
+export default function Principal() {
+    return (
+        <main>
+            <section>
+                <h2>Pagina Inicial</h2>
+            </section>
+            <section>
+                <h2>Meus Pedidos</h2>
+            </section>
+            <section>
+                <h2>Novo Pedidos</h2>
+            </section>
+            <section>
+                <h2>Pagina Sobre</h2>
+            </section>
+        </main>
+    )
+}
