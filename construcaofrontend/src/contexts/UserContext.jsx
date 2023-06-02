5fea7e48ec9be3988181b5c837fd718a1d736b78
unknown <2214290009@academico.iesb.br>
2023-06-02 20:30:50 -0300

    Firebase

--- construcaofrontend/src/contexts/UserContext.jsx
+++ construcaofrontend/src/contexts/UserContext.jsx
@@ -1,15 +1,30 @@
 import { createContext, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVcdAD4breJr2SC2CfyJ3WTlJfKhKE_ts",
+    authDomain: "back-react-a6c06.firebaseapp.com",
+    projectId: "back-react-a6c06",
+    storageBucket: "back-react-a6c06.appspot.com",
+    messagingSenderId: "77655602484",
+    appId: "1:77655602484:web:ef3dbfbb79b4de6a6aa161"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
 
 const UserContext = createContext({
     userID: null,
     logado: false,
-    login: () => {},
-    logout: () => {},
+    login: () => { },
+    logout: () => { },
 })
 
 export function UserContextProvider(props) {
-    const [currentUser, setCurrentUser] = useState({ userID: null, logado: false})
-    
+    const [currentUser, setCurrentUser] = useState({ userID: null, logado: false })
+
     function login() {
         setCurrentUser({ userID: 100, logado: true })
     }
@@ -25,7 +40,7 @@ export function UserContextProvider(props) {
         handleLogout: logout,
     }
 
-    return(
+    return (
         <UserContext.Provider value={contexto}>
             {props.children}
         </UserContext.Provider>
