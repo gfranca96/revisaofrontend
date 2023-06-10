08631d93ad204caa9ae11ec39b17bf985a774b81
unknown <2214290009@academico.iesb.br>
2023-06-09 21:24:34 -0300

    Integração com Realtime Database

---
+++ construcaofrontend/src/pages/NovoForm.jsx
@@ -0,0 +1,39 @@
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { insereTarefa } from '../services/TaskService'
+
+export default function NovoForm() {
+    const { register, handleSubmit } = useForm()
+    const navigate = useNavigate()
+
+    async function onSubmit(data) {
+        try {
+            // Chamar a TaskService
+            await insereTarefa(data)
+            // Navegar para home
+            navigate("/")
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label>Nome da tarefa</label>
+                    <input type="text" {...register("nome")} />
+                </div>
+                <div>
+                    <label>Prioridade</label>
+                    <select {...register("prioridade")}>
+                        <option value="1">Urgente</option>
+                        <option value="2">Importante</option>
+                        <option value="3">Normal</option>
+                    </select>
+                </div>
+                <button>Salvar</button>
+            </form>
+        </>
+    )
+}
